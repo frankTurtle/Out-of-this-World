27d59e2dceb6911aebef5f3eeef1d4aba6357ce5
Barret J. Nobel <bear.nobel@gmail.com>
2015-05-26 10:45:43 -0400

    added cell identifier made it return 1 section with 3 rows set the cell text label

--- Out of this World/OuterSpaceTableViewController.m
+++ Out of this World/OuterSpaceTableViewController.m
@@ -34,24 +34,26 @@
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
 #warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 #warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return 3;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
     
     // Configure the cell...
     
+    cell.textLabel.text = @"woah ... my first tableview";
+    
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
