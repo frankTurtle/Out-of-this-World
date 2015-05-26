921ba6a65414ccbc5344cb5445cdfed67737e2d7
Barret J. Nobel <bear.nobel@gmail.com>
2015-05-26 10:23:33 -0400

    Initial Commit

---
+++ Out of this World/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Out of this World
+//
+//  Created by OSX on 5/26/15.
+//  Copyright (c) 2015 LebonTech solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Out of this World/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Out of this World
+//
+//  Created by OSX on 5/26/15.
+//  Copyright (c) 2015 LebonTech solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
