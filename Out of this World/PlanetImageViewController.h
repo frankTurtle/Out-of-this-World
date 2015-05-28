c40b5b9f0a567365ae1f30053277893c7a894589
Barret J. Nobel <bear.nobel@gmail.com>
2015-05-28 17:27:26 -0400

    created a new viewController class to deal with the new view put a scroll view in our new Planet Image View Controller hooked the scroll view up to a property to make it accessible!

---
+++ Out of this World/PlanetImageViewController.h
@@ -0,0 +1,15 @@
+//
+//  PlanetImageViewController.h
+//  Out of this World
+//
+//  Created by OSX on 5/28/15.
+//  Copyright (c) 2015 LebonTech solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlanetImageViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView; //.... hook up scroll view to storyboard
+
+@end
---
+++ Out of this World/PlanetImageViewController.m
@@ -0,0 +1,37 @@
+//
+//  PlanetImageViewController.m
+//  Out of this World
+//
+//  Created by OSX on 5/28/15.
+//  Copyright (c) 2015 LebonTech solutions. All rights reserved.
+//
+
+#import "PlanetImageViewController.h"
+
+@interface PlanetImageViewController ()
+
+@end
+
+@implementation PlanetImageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
