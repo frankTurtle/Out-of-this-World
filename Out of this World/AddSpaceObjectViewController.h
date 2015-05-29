a687a963b9d068d36250836024f69698185c0099
Barret J. Nobel <bear.nobel@gmail.com>
2015-05-29 19:04:45 -0400

    created an AddSpaceObjectViewControllerDelegate protocol requires you to have methods addSpaceObjecct and didCancel also created a delegate property to use in the tableViewController

--- Out of this World/AddSpaceObjectViewController.h
+++ Out of this World/AddSpaceObjectViewController.h
@@ -8,7 +8,23 @@
 
 #import <UIKit/UIKit.h>
 
+@protocol AddSpaceObjectViewControllerDelegate <NSObject>
+
+// makes you have to use it!!! MMUUAHAHA
+@required
+
+// both methods will be implemented in the OuterSpaceTableViewController
+-(void)addSpaceObject;
+-(void)didCancel;
+
+@end
+
 @interface AddSpaceObjectViewController : UIViewController
+
+// when we've access to the instance inside of the tableViewController we can set this delegate property to self
+// all delegate properties are declared weak so we can deallocate objects when needed
+// id is type unknown that conforms to the AddSpaceObjectViewControllerDelegate
+@property (weak, nonatomic) id <AddSpaceObjectViewControllerDelegate> delegate;
 
 @property (strong, nonatomic) IBOutlet UITextField *nameTextField;
 @property (strong, nonatomic) IBOutlet UITextField *nicknameTextField;
