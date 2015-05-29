7e6fd48955ca2c8dc653683dbc3238e318f0f1f8
Barret J. Nobel <bear.nobel@gmail.com>
2015-05-29 12:00:31 -0400

    added a bg image to the viewController

--- Out of this World/AddSpaceObjectViewController.m
+++ Out of this World/AddSpaceObjectViewController.m
@@ -16,7 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    UIImage *orionImage = [UIImage imageNamed:@"Orion.jpg"];
+    self.view.backgroundColor = [UIColor colorWithPatternImage:orionImage];
 }
 
 - (void)didReceiveMemoryWarning {
