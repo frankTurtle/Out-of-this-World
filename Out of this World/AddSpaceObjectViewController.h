d7589040f59ba2b5a49d4f37dd55c905579f4e4e
Barret J. Nobel <bear.nobel@gmail.com>
2015-05-29 11:47:33 -0400

    added a new viewController added 6 labels 2 buttons hooked them up to the viewController

---
+++ Out of this World/AddSpaceObjectViewController.h
@@ -0,0 +1,21 @@
+//
+//  AddSpaceObjectViewController.h
+//  Out of this World
+//
+//  Created by OSX on 5/29/15.
+//  Copyright (c) 2015 LebonTech solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddSpaceObjectViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UITextField *nameTextField;
+@property (strong, nonatomic) IBOutlet UITextField *nicknameTextField;
+@property (strong, nonatomic) IBOutlet UITextField *diametterTextField;
+@property (strong, nonatomic) IBOutlet UITextField *tempTextField;
+@property (strong, nonatomic) IBOutlet UITextField *noOfMoonsTextField;
+@property (strong, nonatomic) IBOutlet UITextField *interestingFactTextField;
+
+- (IBAction)addButtonClicked:(id)sender;
+- (IBAction)cancelButtonClicked:(id)sender;
+@end
---
+++ Out of this World/AddSpaceObjectViewController.m
@@ -0,0 +1,42 @@
+//
+//  AddSpaceObjectViewController.m
+//  Out of this World
+//
+//  Created by OSX on 5/29/15.
+//  Copyright (c) 2015 LebonTech solutions. All rights reserved.
+//
+
+#import "AddSpaceObjectViewController.h"
+
+@interface AddSpaceObjectViewController ()
+
+@end
+
+@implementation AddSpaceObjectViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)addButtonClicked:(id)sender {
+}
+
+- (IBAction)cancelButtonClicked:(id)sender {
+}
+@end
