b62d327491a160b901d73ea77ce1e27dc337db52
Barret J. Nobel <bear.nobel@gmail.com>
2015-05-29 09:36:20 -0400

    added a cell to the tableview - set its reuseIdentifier to DataCell
    added the delegate methods of cellForRowAtIndexPath
    created a string to hold the cell identifier
    told the tableView's new cell to use the cell with reuseIdentifier of DataCell
    hardcoded the cell's text for now
    
    Also added the numberOfRowsInSection method
    hardcoded value of 5 for now

--- Out of this World/SpaceDataViewController.m
+++ Out of this World/SpaceDataViewController.m
@@ -26,6 +26,24 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - UITableViewDataSource
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"DataCell"; //................................................... create a cell identifier String
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier
+                                                            forIndexPath:indexPath]; //................. creates a tableViewCell, checks the storyboard for a cell named DataCell ( set this in Storyboard )
+    
+    cell.textLabel.text = @"Data"; //................................................................... hard coded label for the cell
+    
+    return cell; //..................................................................................... return the cell
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 5;
+}
+
 /*
 #pragma mark - Navigation
 
