d5cd2f2534635b1548fdc24445ed20f6cdf6a619
Barret J. Nobel <bear.nobel@gmail.com>
2015-05-29 09:18:01 -0400

    added a new viewController and class to manage it put a tableView in the the controller created an IBOutlet for the tableView

---
+++ Out of this World/SpaceDataViewController.h
@@ -0,0 +1,15 @@
+//
+//  SpaceDataViewController.h
+//  Out of this World
+//
+//  Created by OSX on 5/29/15.
+//  Copyright (c) 2015 LebonTech solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpaceDataViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITableView *tableView; //.... new tableView
+
+@end
---
+++ Out of this World/SpaceDataViewController.m
@@ -0,0 +1,37 @@
+//
+//  SpaceDataViewController.m
+//  Out of this World
+//
+//  Created by OSX on 5/29/15.
+//  Copyright (c) 2015 LebonTech solutions. All rights reserved.
+//
+
+#import "SpaceDataViewController.h"
+
+@interface SpaceDataViewController ()
+
+@end
+
+@implementation SpaceDataViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
